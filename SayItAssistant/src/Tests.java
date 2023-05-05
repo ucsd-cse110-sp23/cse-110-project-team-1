57e37a52c044c79e215b8ec1a51e8df0ed83bbd6
philliptwu <ptwu@ucsd.edu>
2023-05-04 18:36:54 -0700

    Added jar for testing and added test file to do testing in

---
+++ SayItAssistant/src/Tests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class Tests {
+    
+    @Test
+    public void UItest() {
+        new SayIt();
+    }
+}
