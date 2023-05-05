e5bddff46b109dbc8fba3a9d449f4f897d3d7c04
skyler <sjgoh@ucsd.edu>
2023-05-05 15:00:29 -0700

    Simplified history class further
    removed JSONArray to just a list of JSONs

--- SayItAssistant/src/History.java
+++ SayItAssistant/src/History.java
@@ -4,19 +4,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Map;
 
-import org.json.simple.JSONArray;
+import org.javatuples.Triplet;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 public class History {
-    private static final String ENTRY_NAME = "Entries";
-    private static final String ID_FIELD = "ID";
-    private static final String QUESTION_FIELD = "Questions";
+    private static final String QUESTION_FIELD = "Question";
     private static final String savePath = "saveFiles/history.json";
-    static final String ANSWER_FIELD = "Answers";
+    static final String ANSWER_FIELD = "Answer";
     static JSONObject saveBody;
-    static JSONArray entries;
+    //static JSONArray entries;
+    static ArrayList<Triplet<Integer,String,String>> entries;
 
     /*
      * Intialize method to be called to recieve all previous prompts and answers
@@ -24,7 +24,8 @@ public class History {
      * Set the saveBody and entries field of this class from the file indicated by savePath
      * if the file is empty, saveBody and entries are defaulted to new 
      */
-    public static void initial() {
+    @SuppressWarnings("unchecked")
+    public static ArrayList<Triplet<Integer,String,String>> initial() {
         //Tries to create new save File if one is not present 
         File save = new File(savePath);
         try {
@@ -34,44 +35,28 @@ public class History {
             io.printStackTrace();
         }
 
+        entries = new ArrayList<>();
         //Parse saveFile and load in previous questions and prompts
         try {
             Object obj = new JSONParser().parse(new FileReader(savePath));
             
             saveBody = (JSONObject) obj;
-
-            entries = (JSONArray) saveBody.get(ENTRY_NAME);
+            
+            Object[] pastPrompts =  saveBody.entrySet().toArray();
+            for (Object j: pastPrompts) {
+                Map.Entry<String,JSONObject> mapEntry = (Map.Entry<String,JSONObject>)j;
+                JSONObject qNa = mapEntry.getValue();
+                int id = Integer.parseInt(mapEntry.getKey());
+                entries.add(new Triplet<Integer,String,String>(id, (String)qNa.get(QUESTION_FIELD), (String)qNa.get(ANSWER_FIELD)));
+            }
         } catch (IOException io) {
             io.printStackTrace();
         } catch (ParseException p) { //If file is empty, default initialize them
             saveBody = new JSONObject();
-            entries = new JSONArray();
         }
+        return entries;
     }
 
-    /* Helper method to get an Entry by ID 
-     * Uses binary search to quickly get entry by ID
-     * @param id of entry to be searched
-    */
-    private static JSONObject getEntryObject(int id) {
-        int left = 0, right = entries.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            JSONObject entry = (JSONObject)entries.get(mid);
-            int entryId = (int)entry.get(ID_FIELD);
-            if (entryId == id) {
-                return entry;
-            }
- 
-            if (entryId < id) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return null;
-    }
     /* Helper method to get an Entry's index in entries array by ID 
      * Uses binary search to quickly get entry by ID
      * @param id of entry to be searched
@@ -81,8 +66,7 @@ public class History {
         while (left <= right) {
             int mid = left + (right - left) / 2;
             
-            JSONObject entry = (JSONObject)entries.get(mid);
-            int entryId = (int)entry.get(ID_FIELD);
+            int entryId = entries.get(mid).getValue0();
             if (entryId == id) {
                 return mid;
             }
@@ -95,16 +79,6 @@ public class History {
         }
         return -1;
     }
-    /* Helper method to format JSON entry into entries array */ 
-    @SuppressWarnings("unchecked")
-    private static JSONObject formatJSONEntry(int id, String prompt, String answer) {
-        JSONObject newEntry = new JSONObject();
-        newEntry.put(ID_FIELD, id);
-        newEntry.put(QUESTION_FIELD, prompt);
-        newEntry.put(ANSWER_FIELD, answer);
-        return newEntry;
-    }
-
     /*
      * Need to call initial first before running this
      * @param prompt: the question to be saved
@@ -112,7 +86,6 @@ public class History {
      * Saves entry into a Json file with the id of array
      * @returns -1 if initial is not called else id of prompt
      */
-    @SuppressWarnings("unchecked")
     public static int addEntry(String prompt, String answer) {
 
         //Check if initial hasn't been called
@@ -120,7 +93,6 @@ public class History {
             return -1;
         } 
 
-        JSONObject lastObject;
         int newId;
 
         //If entry is empty default id to 1
@@ -129,14 +101,12 @@ public class History {
         } else {     
 
         //If entry is not empty, get id from last object + 1                                        
-            lastObject = (JSONObject)entries.get(entries.size()-1);
-            newId = (int)lastObject.get(ID_FIELD) + 1;
+            newId = entries.get(entries.size()-1).getValue0() + 1;
 
         }
 
         //Save to file
-        entries.add(formatJSONEntry(newId, prompt, answer));
-        saveBody.put(ENTRY_NAME, entries);
+        entries.add(new Triplet<Integer,String,String>(newId, prompt, answer));
         writeToFile();
         return newId;
     }
@@ -146,23 +116,31 @@ public class History {
      * @param id of the entry to be deleted
      * removes entry from saveFile
      */
-    @SuppressWarnings("unchecked")
     public static void removeEntry(int id) {
         int index = getEntryIndex(id);  //tries to find entry in array
+        System.out.println("id: " + id + "index: " + index);
         if (index == -1) {  //if not present do nothing and return 
             return;
         }
 
         //remove entry and save to file
         entries.remove(index);
-        saveBody.put(ENTRY_NAME, entries);
+        System.out.println(entries.toString());
         writeToFile();
     }
     
     /*
      * Helper method to write to saveFile
      */
+    @SuppressWarnings("unchecked")
     private static void writeToFile() {
+        saveBody.clear();
+        for (Triplet<Integer,String,String> info: entries) {
+            JSONObject newEntry = new JSONObject();
+            newEntry.put(QUESTION_FIELD, info.getValue1());
+            newEntry.put(ANSWER_FIELD, info.getValue2());
+            saveBody.put(info.getValue0(),newEntry);
+        }
         try {
             PrintWriter pw = new PrintWriter(savePath);
             pw.write(saveBody.toJSONString());
@@ -205,7 +183,6 @@ public class History {
         History.addEntry("What is java UI?", "Idk figure it out bro.");
         History.addEntry("Hi", "bye");
         History.removeEntry(1);
-        History.removeEntry(1);
-        History.clear();
+        History.removeEntry(5);
     }
 }
