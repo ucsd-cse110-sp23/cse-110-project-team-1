d3c6b3de3819fed384e1c881d81c0b168ec10337
philliptwu <ptwu@ucsd.edu>
2023-05-04 18:58:22 -0700

    Added a chatgpt test with a mock chat gpt
    Add different mock questions and answers in the if statement for the MockChatGPT to create more tests?

--- SayItAssistant/src/Tests.java
+++ SayItAssistant/src/Tests.java
@@ -1,12 +1,38 @@
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+
+// Mock ChatGPT returns strings given prompts (feels useless)
+class MockChatGPT extends JChatGPT{
+
+    static String mockRun(String transcription) {
+        if (transcription.equals("What is the smallest city in the world?")) {
+            return "The smallest city in the world is Hum, Croatia. It covers a mere 27 square metres and has a population of just 23 people.";
+        }
+        else {
+            return "bad answer";
+        }
+    }
+}
+
 public class Tests {
-    
+
     @Test
     public void UItest() {
         new SayIt();
     }
+
+    @Test
+    public void ChatGPTtest() {
+        String prompt = "What is the smallest city in the world?";
+        String testAnswer = "The smallest city in the world is Hum, Croatia. It covers a mere 27 square metres and has a population of just 23 people.";
+        String mockAnswer = MockChatGPT.mockRun(prompt);
+        assertEquals(testAnswer, mockAnswer);
+    }
+
 }
