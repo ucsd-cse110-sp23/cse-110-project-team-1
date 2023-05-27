c434fd3322586d1349b79bb9f76a244b0e3d1787
Aaron Li <Aaron8004963@gmail.com>
2023-05-26 21:57:28 -0700

    account field

---
+++ src/LoginScreen.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginScreen extends JFrame {
+    private JTextField accountTextField;
+    private JPasswordField passwordField;
+    private JCheckBox autoLoginCheckbox;
+
+    public LoginScreen() {
+        setTitle("Login Screen");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Terminates the program when the frame is closed
+        setSize(400, 300); // Window size
+        setLocationRelativeTo(null); // Center the window
+
+        // Main Panel to hold components by GridLayout
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(4, 2, 10, 10));
+
+        // Account field
+        JLabel accountLabel = new JLabel("Account:");
+        accountTextField = new JTextField();
+        mainPanel.add(accountLabel);
+        mainPanel.add(accountTextField);
+
+
+
+        // Add the panel to the frame
+        add(mainPanel);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LoginScreen();
+            }
+        });
+    }
+}
